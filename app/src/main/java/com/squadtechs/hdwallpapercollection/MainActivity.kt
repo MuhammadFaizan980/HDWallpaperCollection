4afcc1004bdde39df956577edf3e12ecd7362793
markhor <mohammadfaizan980@gmail.com>
2019-07-29 20:08:59 +0500

    Adjusted theme a bit and added toolbar text

--- app/src/main/java/com/squadtechs/hdwallpapercollection/MainActivity.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollection/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.squadtechs.hdwallpapercollection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        prepareToolbar()
     }
+
+    private fun prepareToolbar() {
+        toolbar.title = "Wallpapers"
+    }
+
+    private fun initViews() {
+        toolbar = findViewById(R.id.toolbar)
+    }
+
 }
