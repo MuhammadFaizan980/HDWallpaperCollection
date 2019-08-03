3823e4ce7976199cf626e9646bae079e1aabc381
markhor <mohammadfaizan980@gmail.com>
2019-08-03 15:26:26 +0500

    added grid row UI XML code

--- app/src/main/java/com/squadtechs/hdwallpapercollection/main_activity/fragment/FragmentMain.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollection/main_activity/fragment/FragmentMain.kt
@@ -1,12 +1,10 @@
 package com.squadtechs.hdwallpapercollection.main_activity.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.squadtechs.hdwallpapercollection.R
 
@@ -21,6 +19,5 @@ class FragmentMain : Fragment() {
         txt.text = arguments!!.getInt("position", -1).toString()
         return view
     }
-
 
 }
