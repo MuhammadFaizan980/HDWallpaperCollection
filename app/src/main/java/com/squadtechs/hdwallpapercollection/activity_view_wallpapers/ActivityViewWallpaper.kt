9e61a1e8e38adf8cdf586c9cb6d76b6dd1bf3a7d
markhor <mohammadfaizan980@gmail.com>
2019-08-04 15:29:45 +0500

    Implemented add to favorite option

--- app/src/main/java/com/squadtechs/hdwallpapercollection/activity_view_wallpapers/ActivityViewWallpaper.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollection/activity_view_wallpapers/ActivityViewWallpaper.kt
@@ -1,6 +1,9 @@
 package com.squadtechs.hdwallpapercollection.activity_view_wallpapers
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
@@ -16,12 +19,41 @@ class ActivityViewWallpaper : AppCompatActivity() {
     private lateinit var viewPager: ViewPager
     private lateinit var viewPagerAdapter: MyPagerAdapter
     private lateinit var list: ArrayList<WallpaperModel>
+    private lateinit var imgFavorite: ImageView
+    private lateinit var imgSetWallpaper: ImageView
+    private lateinit var imgDownloadWallpaper: ImageView
+    private lateinit var pref: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+    private var position = 0
     private val collectionReference = FirebaseFirestore.getInstance().collection("wallpapers")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wiew_wallpaper)
         initViews()
         getData()
+        setViewgaerListener()
+    }
+
+    private fun setViewgaerListener() {
+        viewPager.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(pos: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                position = pos
+                checkFavorite()
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+        })
+    }
+
+    private fun checkFavorite() {
+        if (pref.getBoolean(list[position].wallpaper_key, false)) {
+            imgFavorite.setImageResource(R.drawable.ic_favorite_white)
+        } else {
+            imgFavorite.setImageResource(R.drawable.ic_favorite_holo)
+        }
     }
 
     private fun getData() {
@@ -38,6 +70,9 @@ class ActivityViewWallpaper : AppCompatActivity() {
                     viewPagerAdapter = MyPagerAdapter(list, supportFragmentManager)
                     viewPager.adapter = viewPagerAdapter
                     viewPager.currentItem = intent!!.extras!!.getInt("position")
+                    position = intent!!.extras!!.getInt("position")
+                    checkFavorite()
+                    setListener()
                 } else {
                     Toast.makeText(this, task.exception!!.message!!, Toast.LENGTH_LONG).show()
                 }
@@ -47,6 +82,27 @@ class ActivityViewWallpaper : AppCompatActivity() {
     private fun initViews() {
         viewPager = findViewById(R.id.viewpager_view_wallpaper)
         list = ArrayList()
+        imgFavorite = findViewById(R.id.img_favorite)
+        imgSetWallpaper = findViewById(R.id.img_set_wallpaper)
+        imgDownloadWallpaper = findViewById(R.id.img_download_wallpaper)
+        pref = getSharedPreferences("favorite", Context.MODE_PRIVATE)
+        editor = pref.edit()
+    }
+
+    private fun setListener() {
+        imgFavorite.setOnClickListener {
+            if (pref.getBoolean(list[position].wallpaper_key, false)) {
+                imgFavorite.setImageResource(R.drawable.ic_favorite_holo)
+                editor.putBoolean(list[position].wallpaper_key, false)
+                editor.apply()
+            } else {
+                imgFavorite.setImageResource(R.drawable.ic_favorite_white)
+                editor.putBoolean(list[position].wallpaper_key, true)
+                editor.apply()
+            }
+        }
+        imgSetWallpaper.setOnClickListener { }
+        imgDownloadWallpaper.setOnClickListener { }
     }
 
 }
--- app/src/main/java/com/squadtechs/hdwallpapercollection/activity_view_wallpapers/FragmentViewWallpaper.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollection/activity_view_wallpapers/FragmentViewWallpaper.kt
@@ -4,15 +4,57 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.android.volley.Response
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.Volley
 import com.squadtechs.hdwallpapercollection.R
 
 class FragmentViewWallpaper : Fragment() {
+    private lateinit var imgViewWallPaper: ImageView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var url: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_fragmet_view_wallpaper, container, false)
+        val view = inflater.inflate(R.layout.fragment_fragmet_view_wallpaper, container, false)
+        initView(view)
+        loadImage()
+        return view
     }
+
+    private fun loadImage() {
+        val requestQueue = Volley.newRequestQueue(activity!!.applicationContext)
+        val imageRequest = ImageRequest(
+            url,
+            Response.Listener { response ->
+                progressBar.visibility = View.GONE
+                imgViewWallPaper.setImageBitmap(response)
+            },
+            0,
+            0,
+            ImageView.ScaleType.CENTER,
+            null,
+            Response.ErrorListener { error ->
+                progressBar.visibility = View.GONE
+                Toast.makeText(
+                    activity!!.applicationContext,
+                    "There was an error loading this wallpaper",
+                    Toast.LENGTH_LONG
+                ).show()
+            })
+        requestQueue.add(imageRequest)
+    }
+
+    private fun initView(view: View) {
+        imgViewWallPaper = view.findViewById(R.id.img_view_wallpaper)
+        progressBar = view.findViewById(R.id.progress_view_image)
+        url = arguments!!.getString("url")!!
+    }
+
 }
